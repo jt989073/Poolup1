ef20e17353481e728e8c8cb46461b2f1f7f45ed8
James Thompson <thompsonjames0120@gmail.com>
2021-10-08 00:14:17 -0500

    finished part 2

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,4 +1,5 @@
-const { validationResult } = require("express-validator");
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
@@ -6,12 +7,14 @@ const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validationErrors.array().map((error) => `${error.msg}`);
+    const errors = validationErrors
+      .array()
+      .map((error) => `${error.msg}`);
 
-    const err = Error("Bad request.");
+    const err = Error('Bad request.');
     err.errors = errors;
     err.status = 400;
-    err.title = "Bad request.";
+    err.title = 'Bad request.';
     next(err);
   }
   next();
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import sessionReducer from './session';
+
 const rootReducer = combineReducers({
-});
+    session: sessionReducer,
+  });
 
 let enhancer;
 
@@ -16,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
-};
+    return createStore(rootReducer, preloadedState, enhancer);
+  };
 
-export default configureStore;
+  export default configureStore;
