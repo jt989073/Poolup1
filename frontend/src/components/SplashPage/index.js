be05221aeaf007a7954fd45c0e1040e3add93cfe
James Thompson <thompsonjames0120@gmail.com>
2021-10-10 13:42:24 -0500

    background image changed

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,8 +6,8 @@ export default function Splash (){
 
 
   return (
-    <div className="splash-body">
-      <img src="https://res.cloudinary.com/dqwy6sxtc/image/upload/v1633834797/poolupp/pen-tsai-yUpn_S_x5BU-unsplash_qdlgct.jpg" className="splash-image" alt="" />
+    <div className="splash-image">
+      <main className="splash-main"></main>
     </div>
   )
 }
